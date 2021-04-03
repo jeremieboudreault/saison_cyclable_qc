0157b7977af7cc58aeff94a9416150591bf10df2
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-04-03 12:22:59 -0400

    Added names of the weather data columns in internals.R.

--- R/internals.R
+++ R/internals.R
@@ -35,6 +35,49 @@ names_stns <- c(
     "YEAR_END_M"
 )
 
+# Names for the weather data files.
+names_weather_data <- list(
+
+    # Geo -------------------------
+    LONGITUDE           = "GEO",
+    LATITUDE            = "GEO",
+
+    # IDs -------------------------
+    NAME                = "ID",
+    CLIMATEID           = "ID",
+
+    # Dates -----------------------
+    DATETIME            = "DATE",
+    YEAR                = "DATE",
+    MONTH               = "DATE",
+    DAY                 = "DATE",
+
+    # Values and flags ------------
+    DATAQUALITY         = "FLAG",
+    TEMP_MAX            = "VALUE",
+    TEMP_MAX_FLAG       = "FLAG",
+    TEMP_MIN            = "VALUE",
+    TEMP_MIN_FLAG       = "FLAG",
+    TEMP_MEAN           = "VALUE",
+    TEMP_MEAN_FLAG      = "FLAG",
+    HEAT_DAYS_DEG       = "VALUE",
+    HEAT_DAYS_DEG_FLAG  = "FLAG",
+    COOL_DAYS_DEG       = "VALUE",
+    COOL_DAYS_DEG_FLAG  = "FLAG",
+    RAIN_TOT            = "VALUE",
+    RAIN_TOT_FLAG       = "FLAG",
+    SNOW_TOT            = "VALUE",
+    SNOW_TOT_FLAG       = "FLAG",
+    PREC_TOT            = "VALUE",
+    PREC_TOT_FLAG       = "FLAG",
+    SNOW_GRND           = "VALUE",
+    SNOW_GRND_FLAG      = "FLAG",
+    MAX_GUST_DIR        = "VALUE",
+    MAX_GUST_DIR_FLAG   = "FLAG",
+    MAX_GUST_SPEED      = "VALUE",
+    MAX_GUST_SPEED_FLAG = "FLAG"
+)
+
 
 # Internal functions -----------------------------------------------------------
 
